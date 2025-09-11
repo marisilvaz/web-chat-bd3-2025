e64c70e5194d54ad4a00e05fbf0871451558d8f9
Mariana Evangelista <maryevang.mx@gmail.com>
2025-09-11 16:23:23 -0300

    Atualizando o webchat

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const ejs = require("ejs");
 const http = require("http");
 const path = require("path");
 const socketIO = require("socket.io");
-
 const app = express();
-
 const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //__dirname é tudo que está atras da raiz do projeto
 
 app.set("views", path.join(__dirname, "public"));
 
@@ -21,3 +20,16 @@ app.use("/", (req, res) => {
 server.listen(3000, () => {
   console.log("SERVIDOR RODANDO EM - http://localhost:3000");
 });
+
+let messagens = [];
+
+io.on('connection', socket =>{
+  console.log('ID de usuário conectado: ' + socket.id);
+  socket.emit('previousMessage', messagens);
+  socket.on('sendMessage', data =>{
+    messagens.push(data); //estilo de fila
+    socket.broadcast.emit('receiveddMessage', data);
+  });
+}); //ligar
+
+
